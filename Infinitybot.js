3683aabb2dd84c201056ef82880b0791a04e9bc9
John Graves <johntaylorgraves@gmail.com>
2018-06-19 19:48:27 +0100

    First JS file!

---
+++ Infinitybot.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+// bot ready
+bot.on("ready", () => {
+    console.log("InfinityBot Online");
+  });
+
+  // login token
+bot.login("NDU4NzAyODg2NTgzNDAyNDk2.Dgrf7w.XlqtoQApub0gPqI0nhaqDY1aGdk");
