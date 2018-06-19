1d76f0ef63b5282cb99257d920a1b29f04ad469d
John Graves <johntaylorgraves@gmail.com>
2018-06-19 20:26:08 +0100

    added the test command and tokens

--- Infinitybot.js
+++ Infinitybot.js
@@ -1,10 +1,27 @@
-const Discord = require("discord.js");
+var fs = require('fs');
+
+const Discord = require("Discord.js");
 const bot = new Discord.Client();
+const token = require("C:/token/token.json");
 
 // bot ready
 bot.on("ready", () => {
     console.log("InfinityBot Online");
   });
 
+// Auto-reconnect
+bot.on('disconnect', function(erMsg, code) {
+  console.log('----- Bot disconnected from Discord with code', code, 'for reason:', erMsg, '-----');
+  bot.connect();
+});
+
+// test command
+bot.on("message", (message) => {
+  if (message.content.startsWith("!",0) && message.content.match("test")) {
+    message.channel.send("success!");
+    console.log("test");
+  }
+});
+
   // login token
-bot.login("NDU4NzAyODg2NTgzNDAyNDk2.Dgrf7w.XlqtoQApub0gPqI0nhaqDY1aGdk");
+bot.login(token.token);
