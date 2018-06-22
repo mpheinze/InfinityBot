d2dd837b4e4d9b3f14b3273a0ebeebac0634a011
mpheinze <heinze89@hotmail.com>
2018-06-23 00:14:28 +0200

    fixed date of upcoming event

--- testscraper.js
+++ testscraper.js
@@ -1,12 +1,12 @@
 ///// -----     Next Upcoming Event Scraper     ----- /////
 
-var request = require('request');
+let request = require('request');
 
 // Calculating current year and month for dynamic url
-var current_date = new Date();
-var month = ('0' + (current_date.getMonth() + 1)).slice(-2);
-var year = current_date.getFullYear();
-var url = `https://superinfinityfriends.shivtr.com/events?date=${year}-${month}-01`
+let current_date = new Date();
+let month = ('0' + (current_date.getMonth() + 1)).slice(-2);
+let year = current_date.getFullYear();
+let url = `https://superinfinityfriends.shivtr.com/events?date=${year}-${month}-01`
 
 // Scraping event calendar
 request({
@@ -14,35 +14,35 @@ request({
     json: true
 }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-        var date_now = Date.now();
-        var event_date_list = [];
+        
+        let date_now = Date.now();
+        let event_date_list = [];
+        let event_diff_list = [];
+        
         for (event of body.events) {
             let date = new Date(event.date);
-            event_date_list.push(date - date_now);
+            event_date_list.push(date);
+            event_diff_list.push(date - date_now);
         }
 
-        // Getting ID for next event in the calendar
-        upcoming_list = event_date_list.filter(function(i) {return i > -1} );
-        var next_event = body.events[event_date_list.length - upcoming_list.length];
-        var next_event_id = next_event.id;
-        var next_event_url = `https://superinfinityfriends.shivtr.com/events/869326?event_instance_id=${next_event_id}`;
-        console.log(next_event_url);
+        // Getting index of next event and ID for event specific url
+        let date_index = event_diff_list.length - event_diff_list.filter(function(i) {return i > -1} ).length;
+        let next_event = body.events[date_index];
+        let next_event_id = next_event.id;
+        let next_event_url = `https://superinfinityfriends.shivtr.com/events/869326?event_instance_id=${next_event_id}`;
 
-        // Sracping next event page
+        // Sracping event specific event page
         request({
             url: next_event_url,
             json: true
         }, function (error, response, event_info) {
             if (!error && response.statusCode === 200) {
 
+                event_info.event.date = event_date_list[date_index];
                 console.log(event_info);
-
+                
             }
         })
 
     }
 })
-
-
-
-
