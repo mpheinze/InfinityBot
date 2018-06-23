85d793321b3bd5ec7ffece72c8f0b3d364f42b69
mpheinze <heinze89@hotmail.com>
2018-06-23 16:26:16 +0200

    added nextraid command

---
+++ commands/htmlscraper.js
@@ -0,0 +1,54 @@
+///// -----     Next Upcoming Event Scraper     ----- /////
+
+const request = require('request');
+
+function next_event_scraper(callback) {
+
+    // Calculating current year and month for dynamic url
+    let current_date = new Date();
+    let month = ('0' + (current_date.getMonth() + 1)).slice(-2);
+    let year = current_date.getFullYear();
+    let url = `https://superinfinityfriends.shivtr.com/events?date=${year}-${month}-01`
+
+        // Scraping event calendar
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                
+                let date_now = Date.now();
+                let event_date_list = [];
+                let event_diff_list = [];
+                
+                for (event of body.events) {
+                    let date = new Date(event.date);
+                    event_date_list.push(date);
+                    event_diff_list.push(date - date_now);
+                }
+
+                // Getting index of next event and ID for event specific url
+                let date_index = event_diff_list.length - event_diff_list.filter(function(i) {return i > -1} ).length;
+                let next_event = body.events[date_index];
+                let event_id = next_event.event_id;
+                let instance_id = next_event.id;
+                let next_event_url = `https://superinfinityfriends.shivtr.com/events/${event_id}?event_instance_id=${instance_id}`;
+
+                // Sracping event specific event page
+                request({
+                    url: next_event_url,
+                    json: true
+                }, function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        body.event.date = event_date_list[date_index];
+                        body.event.instance_id = instance_id;
+                        body.event.event_url = next_event_url;
+                        event_data = body;
+                        callback(event_data);
+                    }
+                })
+            }
+        })
+}
+
+module.exports.next_event_scraper = next_event_scraper;
---
+++ commands/nextraid.js
@@ -0,0 +1,61 @@
+///// ----- InfinityBot - !nextraid ----- /////
+
+// Command syntax: !nextraid
+// Shows next upcoming calendar event as an embed
+
+const Scraper = new require('./htmlscraper');
+const dateFormat = require('dateformat');
+
+exports.run = async (client, message, args, level) => {
+
+    // Importing html data from scraper
+    Scraper.next_event_scraper(function(event_data) {
+    
+        // formatting date and time strings
+        let event_date = dateFormat(event_data.event.date, 'dd-mm-yyyy');
+        let event_time = `${dateFormat(event_data.event.date.setHours(event_data.event.date.getHours() - 2), 'HH:MM')} ST`;
+    
+        message.channel.send({embed: {
+            color: 3447003,
+            author: {
+                name: 'Next upcoming raid:',
+                icon_url: message.guild.iconURL
+              },
+            title: `__**${event_data.event.name}**__`,
+            url: event_data.event.event_url,
+            description: `Date: ${event_date}. Time: ${event_time}`,
+            fields: [{
+                name: 'Description',
+                value: event_data.event.description
+              }
+            ],
+            timestamp: new Date(),
+            footer: {
+                icon_url: client.user.avatarURL,
+                text: "© Powered by InfinityBot"
+              }
+        }});
+
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
