2f8ad2a75eb3ed167ec14cb25372ca5570837fe3
mpheinze <heinze89@hotmail.com>
2018-06-24 14:25:09 +0200

    fixed time conversion

--- commands/nextraid.js
+++ commands/nextraid.js
@@ -13,9 +13,9 @@ exports.run = async (client, message, args, level) => {
     Scraper.next_event_scraper(function(event_data) {
     
         // formatting date and time strings
-        let event_date = dateFormat(event_data.event.date, 'dd-mm-yyyy');
-        let event_time = `${dateFormat(event_data.event.date.setHours(event_data.event.date.getHours() - 2), 'HH:MM')} ST`;
-    
+        let event_time = event_data.event.date.toUTCString();
+        event_time = `${event_time.substring(0, event_time.length - 7)} ST`
+        
         message.channel.send({embed: {
             color: 2190157,
             author: {
@@ -24,11 +24,14 @@ exports.run = async (client, message, args, level) => {
               },
             title: `__**${event_data.event.name}**__`,
             url: event_data.event.event_url,
-            description: `Date: ${event_date}. Time: ${event_time}`,
             fields: [{
+                name: 'Date and time',
+                value: event_time
+                },
+                {
                 name: 'Description',
-                value: event_data.event.description
-              }
+                value: `${event_data.event.description}`
+                }
             ],
             timestamp: new Date(),
             footer: {
