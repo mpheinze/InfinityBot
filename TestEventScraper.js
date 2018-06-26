498ec771d3de25733a7dda0bc488f96679eea31c
mpheinze <heinze89@hotmail.com>
2018-06-26 03:27:23 +0200

    fixed few typos and added comments to TestEventScraper

--- TestEventScraper.js
+++ TestEventScraper.js
@@ -19,7 +19,7 @@ request(url, function (error, response, body) {
         var $ = cheerio.load(body); // not sure why, but $ seems to be used as the variable name in cherrio by convention
         
         // Scraping sign-up index, name, and class. Mainly using native js string functions like split(), trim(), and slice() to get the right format of the text in the html
-        $('.ellipsis').each(function(i, element){ // ellipsis is the relevant html object to look for
+        $('.ellipsis').each(function(){ // 'ellipsis' is the relevant html object to look for
             var item = $(this);
             index_list.push(parseInt(item.text().split('\n')[1].trim().slice(0, -1)));
             name_list.push(item.text().split('\n')[3].trim());
@@ -27,7 +27,7 @@ request(url, function (error, response, body) {
         });
 
         // Getting role counts (i.e. number of tanks, dps, and healers) signed up
-        $('.table_box_pad').each(function(i, element){ // here table_box_pad is not the exact heml element, but the one after what we want, thus the .prev() method in the next line
+        $('.table_box_pad').each(function(){ // here 'table_box_pad' is not the exact html element, but the one after what we want, thus the .prev() method in the next line
             var item = $(this).prev();
             role_counts.push(parseInt(item.text().split('\n')[2].trim().slice(1, -1)));
         });
