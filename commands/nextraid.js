dbb8b1e6e04752ca33dd6bfdd30eea6333d4387d
mpheinze <heinze89@hotmail.com>
2018-06-28 16:52:50 +0200

    added new scraper and nextraid command

--- commands/nextraid.js
+++ commands/nextraid.js
@@ -4,42 +4,35 @@
 // Arguments: 0
 // Shows next upcoming calendar event as an embed
 
-const Scraper = new require('./htmlscraper');
-const dateFormat = require('dateformat');
+const Discord = require("Discord.js");
+const EventScraper = new require('./nextevent_scraper');
 
 exports.run = async (client, message, args, level) => {
+    EventScraper.event_scraper(function(event_data) {
 
-    // Importing html data from scraper
-    Scraper.next_event_scraper(function(event_data) {
-    
-        // formatting date and time strings
-        let event_time = event_data.event.date.toUTCString();
-        event_time = `${event_time.substring(0, event_time.length - 7)} ST`
-        
-        message.channel.send({embed: {
-            color: 2190157,
-            author: {
-                name: 'Next upcoming raid:',
-                icon_url: message.guild.iconURL
-              },
-            title: `__**${event_data.event.name}**__`,
-            url: event_data.event.event_url,
-            fields: [{
-                name: 'Date and time',
-                value: event_time
-                },
-                {
-                name: 'Description',
-                value: `${event_data.event.description}`
-                }
-            ],
-            timestamp: new Date(),
-            footer: {
-                icon_url: client.user.avatarURL,
-                text: "© Powered by InfinityBot"
-              }
-        }});
+        // Formatting date and time strings
+        let event_date = new Date(event_data.meta_data.date).toUTCString();
+        event_date = `${event_date.substring(0, event_date.length - 7)} ST \n`;
 
-    });
+        // Creating embed object 
+        const embed = new Discord.RichEmbed()
+            .setTitle(event_data.meta_data.title)
+            .setURL(event_data.meta_data.url)
+            .setAuthor("Super Infinity Friends' Next Raid:", message.guild.iconURL)
+            .setColor(2190157)
+            .setFooter('© Your friendly neighborhood InfinityBot', client.user.avatarURL)
+            .setTimestamp()
+            .addField('__Date and time__', event_date)
+            .addField('__Sign-ups__',
+                `Total:         ${event_data.meta_data.signup_total} (${event_data.meta_data.signup_maybe} maybe)\n` + 
+                `Tanks:          ${event_data.meta_data.role_count.tank}\n` +
+                `Damage:    ${event_data.meta_data.role_count.damage}\n` +
+                `Healers:       ${event_data.meta_data.role_count.healer}`
+            )
+            .addField('__Description__', event_data.meta_data.description)
 
+        // Sending embed to channel
+        message.channel.send({embed});
+
+    })
 }
