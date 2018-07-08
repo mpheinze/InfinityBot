b48b37133e2b85386ed9cf4480e5d1458b950e50
John Graves <johntaylorgraves@gmail.com>
2018-07-08 23:10:14 +0100

    added embed and renamed to cmdlist

---
+++ commands/cmdlist.js
@@ -0,0 +1,23 @@
+/* COMMANDLIST COMMAND */
+
+const fs = require('fs');   
+const Discord = require('Discord.js');
+
+exports.run = async (client, message, args, level) => {
+
+    var i = 0;
+    var msgString = '';
+
+    await fs.readdir('./commands/', (err, files) => {
+        if (err) throw err;
+        for (i=0;i<files.length;i++) {
+            files[i] = files[i].slice(0,-3);
+            msgString += files[i] + ', ';
+        };
+        const embed = new Discord.RichEmbed()
+        embed.setTitle('__InfinityBot Commandlist:__');
+        embed.setDescription(msgString.slice(0,-2));
+         console.log({embed});
+         message.channel.send({embed});
+    });
+}
