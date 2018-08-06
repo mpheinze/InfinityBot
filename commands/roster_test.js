724b06dcfc812abf579f7d89e5bfa05aaeffb9d1
John Graves <johntaylorgraves@gmail.com>
2018-08-06 22:33:53 +0100

    removed commands that are being tested or could be abused

--- commands/roster_test.js
+++
@@ -1,8 +0,0 @@
-/* MEMBERS COMMAND */
-
-const scraper = new require('../util/roster_scraper');
-
-exports.run = async (client, message, args, level) => {
-  msgString = await scraper.rosterScraper(message, args);
-  message.channel.send(msgString);
-}
